9464a7a5783ce4f1a3187d41aacc3db37fdbb2b0
WinPlay02 <winplay02_gh@woberlaender.de>
2017-03-28 20:29:03 +0200

    added mod-container for the api

---
+++ src/main/java/net/wfoas/gh/omapi/GHAPIModContainer.java
@@ -0,0 +1,13 @@
+package net.wfoas.gh.omapi;
+
+import net.minecraftforge.fml.common.Mod;
+import net.wfoas.gh.GameHelper;
+
+@Mod(modid = GHAPIModContainer.MODID, name = GHAPIModContainer.MODNAME, version = GHAPIModContainer.MODVER)
+public class GHAPIModContainer {
+
+	public static final String MODID = "gamehelper-api";
+	public static final String MODNAME = "GameHelper-API";
+	public static final String MODVER = "0.1.2-b";
+
+}
